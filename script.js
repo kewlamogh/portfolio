3c4d244a9eb41bd309d0b70b1916ca39e6ae5ecc
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-07-06 17:53:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ async function slowType(what, onwhichhtmlelement, startWait) {
   }
 }
 slowType('Hey, my name is Amogh (TheCool?)! ', document.getElementById('myname'), 0);
-document.getElementById('myname').innerHTML += '<img src = "mrshakey.jpg" id = "em"> </img>';
+document.getElementById('myname').innerHTML += '<img src = "mrshakey (1).jpg" id = "em"> </img>';
 slowType('I am a [kid] web and Android developer who is obsessed with making games & playing a game called Halo. My hobbies include gaming, coding, drawing, and breaking things that I build with Legos. I am moderately skilled with HTML, vanilla (no framework/module) JS, Python, CSS, Kotlin, Markdown (not sure that counts...), and am learning backend dev with Golang.', document.getElementById('whatido'),'Hey, my name is Amogh (TheCool?)! '.length * 75); /*This part starts typing this after typing the first intro... I used diferrent HTMLElements*/;
 document.getElementById('whatido').innerHTML += '<br><br>'; //Inserting line breaks
